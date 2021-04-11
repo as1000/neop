3604dc2b285e1b27016c62b9845a3cc61ec63a77
as1000 <32502256+as1000@users.noreply.github.com>
2021-04-10 21:03:46 -0400

    Update annotate_maf_test.py

--- tests/annotate_maf_test.py
+++ tests/annotate_maf_test.py
@@ -18,12 +18,12 @@ if not os.path.isdir(OUTPUT_FOLDER):
 maf_filename_input = os.path.join(path, "test_maf.maf")
 log_file_input = os.path.join(path, "log_file.log")
 
-with open(os.path.join(OUTPUT_FOLDER, "annotate_maf_test.maf")) as f:
+with open(os.path.join(OUTPUT_FOLDER, "annotate_maf_test.maf"), "r") as f:
     original_file_content = f.read()
 
 output_maf = annotate_maf(maf_filename_input, OUTPUT_FOLDER, "annotate_maf_test")
 
-with open(output_maf, "a+") as f:
+with open(output_maf, "r") as f:
     generated_file_content = f.read()
 
 assert generated_file_content == original_file_content
