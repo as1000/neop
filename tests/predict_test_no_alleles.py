a14aa14c07539acf64795171ccf619bea542d1e3
as1000 <32502256+as1000@users.noreply.github.com>
2021-04-10 21:13:41 -0400

    Update predict_test_no_alleles.py

--- tests/predict_test_no_alleles.py
+++ tests/predict_test_no_alleles.py
@@ -20,4 +20,18 @@ maf_filename_input = os.path.join(path, "test_maf.maf")
 
 allele_annot = os.path.join(path, "allele_ref.csv")
 
+with open(os.path.join(OUTPUT_FOLDER, "predict_test_no_alleles_output.csv"), "r") as f:
+    original_csv_content = f.read()
+
+with open(os.path.join(OUTPUT_FOLDER, "predict_test_no_alleles_output.json"), "r") as f:
+    original_json_content = f.read()
+
 output_json, output_csv = predict("", maf_filename_input, OUTPUT_FOLDER, "predict_test_no_alleles")
+
+with open(output_csv, "r") as f:
+    generated_csv_content = f.read()
+
+with open(output_json, "r") as f:
+    generated_json_content = f.read()
+
+assert original_csv_content == generated_csv_content and original_json_content == generated_json_content
